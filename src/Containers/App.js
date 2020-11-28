3a072511074ae2f2c5304b977bb35500a0f55fa9
pzey2011 <peymanzeynali@yahoo.com>
2020-11-28 17:10:42 +0330

    adding reset styles and fontawesome font icons and project structure is optimized

---
+++ src/Containers/App.js
@@ -0,0 +1,11 @@
+import '../Assets/Styles/App.css';
+
+function App() {
+  return (
+    <div>
+      citation app
+    </div>
+  );
+}
+
+export default App;
--- src/index.js
+++ src/index.js
@@ -1,8 +1,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
+import App from './Containers/App';
 import reportWebVitals from './reportWebVitals';
+import './Assets/Styles/fonts.css';
+import './Assets/Styles/fontawesome.min.css';
+import './Assets/Styles/index.css';
 
 ReactDOM.render(
   <React.StrictMode>
